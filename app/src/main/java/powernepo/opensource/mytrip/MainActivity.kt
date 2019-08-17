8a63dfe95bde368276609639534d10d23e168b2c
FAJ_PC <lucas.nepomuceno1999@gmail.com>
2019-08-17 11:08:56 -0300

    Logic basic and constrution of methods

--- app/src/main/java/powernepo/opensource/mytrip/MainActivity.kt
+++ app/src/main/java/powernepo/opensource/mytrip/MainActivity.kt
@@ -2,11 +2,46 @@ package powernepo.opensource.mytrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_calculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        if(id == R.id.btn_calculate) handleCalculate()
+
+    }
+
+    private fun handleCalculate(){
+        if(isValid()){
+            try{
+                val distance = edt_distance.text.toString().toFloat()
+                val price = edt_price.text.toString().toFloat()
+                val autonomy = edt_autonomy.text.toString().toFloat()
+
+                val result = ((distance * price) / autonomy)
+                textResult.setText("TOTAL : R$ $result")
+            }catch (err: NumberFormatException){
+                Toast.makeText(this," ERRO :impossible to calculate letters",Toast.LENGTH_LONG)
+            }
+        }else Toast.makeText(this," ERRO : inform values correct",Toast.LENGTH_LONG)
+
+    }
+
+    private fun isValid(): Boolean{
+        return ((edt_distance.text.toString() != "") && edt_price.text.toString() != "" &&
+                (edt_autonomy.text.toString() != "" || edt_autonomy.text.toString() != ""))
     }
 }
